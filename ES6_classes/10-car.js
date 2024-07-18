5751c1fa35e7006460eef805704c0c1fdf551184
Abimael <icemask2612@gmail.com>
2024-07-18 18:29:55 -0400

    fixing eslint

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,27 +1,27 @@
 export default class Car {
-    constructor(brand, motor, color) {
-      this._brand = brand;
-      this._motor = motor;
-      this._color = color;
-    }
-  
-    get brand() {
-      return this._brand;
-    }
-  
-    get motor() {
-      return this._motor;
-    }
-  
-    get color() {
-      return this._color;
-    }
-  
-    cloneCar() {
-      const clone = Object.create(Object.getPrototypeOf(this));
-      Object.assign(clone, this);
-      return clone;
-    }
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
-  
-  Car[Symbol.species] = Car;
+
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  cloneCar() {
+    const clone = Object.create(Object.getPrototypeOf(this));
+    Object.assign(clone, this);
+    return clone;
+  }
+}
+
+Car[Symbol.species] = Car;
