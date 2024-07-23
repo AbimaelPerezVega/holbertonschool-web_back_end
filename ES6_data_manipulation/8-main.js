63aaf58eebc90e1fc64aa790dd8935c76a2f2177
Abimael <icemask2612@gmail.com>
2024-07-23 17:35:54 -0400

    pushing all

---
+++ ES6_data_manipulation/8-main.js
@@ -0,0 +1,6 @@
+// 8-main.js
+
+import cleanSet from './8-clean_set.js';
+
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));
