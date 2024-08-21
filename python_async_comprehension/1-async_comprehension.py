704193f3301c3c0760f6f3529fce5814b3ec5aa5
Abimael <icemask2612@gmail.com>
2024-08-21 14:42:41 -0400

    fix 1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -7,7 +7,9 @@ from typing import List
 # Importing the async_generator function
 task_0_function = __import__('0-async_generator').async_generator
 
+
 async def async_comprehension() -> List[float]:
-    """Collects 10 random numbers from async_generator using an async comprehension."""
+    """Collects 10 random numbers from async_generator
+    using an async comprehension."""
 
     return [i async for i in task_0_function()]
