548613d66d2a29c7271858cb7be187df8701b6e5
Abimael <icemask2612@gmail.com>
2024-08-21 14:29:01 -0400

    all the async

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""This module contains the async_comprehension coroutine."""
+
+import asyncio
+from typing import List
+
+# Importing the async_generator function
+task_0_function = __import__('0-async_generator').async_generator
+
+async def async_comprehension() -> List[float]:
+    """Collects 10 random numbers from async_generator using an async comprehension."""
+
+    return [i async for i in task_0_function()]
