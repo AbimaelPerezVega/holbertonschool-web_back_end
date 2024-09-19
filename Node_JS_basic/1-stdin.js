d5ee509daa6f7b4a2adb10530b5608516f9e64fe
Abimael <icemask2612@gmail.com>
2024-09-19 15:19:07 -0400

    help

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,18 +1,12 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Set up the input/output interface
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+process.stdin.on('data', (input) => {
+  const name = input.toString().trim();
 
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
   console.log(`Your name is: ${name}`);
-  rl.close();
-});
 
-// Detect when the interface is closed and print the closing message
-rl.on('close', () => {
-  console.log('This important software is now closing');
-  process.exit(0);
+  if (!process.stdin.isTTY) {
+    console.log('This important software is now closing');
+  }
+  process.exit();
 });
