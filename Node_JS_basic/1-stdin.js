0c31e27ea469b61f98c4e0897a315c19b39fae7c
Abimael <icemask2612@gmail.com>
2024-09-19 15:47:36 -0400

    not finished and neither is 8

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,13 +3,10 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (input) => {
   const name = input.toString().trim();
 
-  // Output the name without automatically adding a new line
-  process.stdout.write(`Your name is: ${name}\r\n`);
+  console.log(`Your name is: ${name}`);
 
-  // Check if input is coming from a non-TTY (piped input)
   if (!process.stdin.isTTY) {
-    process.stdout.write('This important software is now closing\r\n');
+    console.log('This important software is now closing');
   }
-
   process.exit();
 });
